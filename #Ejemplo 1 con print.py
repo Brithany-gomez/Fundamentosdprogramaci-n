d0f73d3718f6707bfae10cca9dba90b26836a391
Brithany-gomez <al03038381@tecmilenio.mx>
2025-10-23 07:26:00 -0600

    Add files via upload

---
+++ #Ejemplo 1 con print.py
@@ -0,0 +1,70 @@
+#Ejemplo 1 con print
+
+print("Ejemplo 1\n")
+
+# Instrucción 1
+nombre = "Brithany" 
+
+# Instrucción 2
+edad = 18
+
+# Instrucción 3
+print("hola, soy", nombre)
+
+# Instrucción 4
+print("Tengo", edad, "años")
+
+
+#Ejemplo 2 con print
+#2. Nueva Línea (\n)
+print("\nEjemplo 2\n")
+
+# Sin \n (todo en una línea)
+print("Hola Mundo")
+# Salida: Hola Mundo
+
+# Con \n (salto de línea)
+print("Hola\nMundo")
+
+# Múltiples \n
+print("línea 1\nLínea 3")
+
+#Ejemplo 3 con tabulación
+print("\nEjemplo 3\n")
+
+print("Nombre:\tBrithany")
+print("Edad:\t18")
+print("Ciudad:\tMonterrey")
+
+#Ejemplo 4 con tabulación
+print("\nEjemplo 4\n")
+
+#Crear una conversación de chat
+print("Btith:\tHola, ¿cómo estás?")
+print("Fer:\tBien, ¿y tu?")
+print("Brith:\t¡Genial!\n")
+print("===== Chat guardado en =====")
+print("C:\\Users\\NOMBRE_TU_CARPETA\\Documents\\chat.txt")
+
+
+
+# Ejemplo 5
+print("\nEjemplo 5\n")
+
+# Dos argumentos
+print("hola","Mundo")
+# Salida: Hola Mundo
+#           ↑
+#           espacio automático
+
+# Tres argumentos
+print("Me","gusta","dibujar")
+
+# Mezclando tipo de datos
+print("Tengo", 2, "mascotas en mi casa")
+
+# Con variables
+nombre = "Brith"
+cantidad_hermanos = 4
+print("Me dicen", nombre, "y tengo", cantidad_hermanos, "hermanos")
+
